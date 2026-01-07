17f6fc85eae63a8129162629b7458081df02d8ad
Gaurav Thapa <102647014+gauravthapamagar@users.noreply.github.com>
2026-01-07 23:56:35 +0545

    backend blogs modified

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/blog/', include('blogs.urls')),
     path('api/', include('accounts.urls')),
     path('api/survey/', include('surveys.urls')),
 ]
